97b52b38476b96c48c05635f8f5fd8e1567a6bc0
冯景琦 <1563771456@qq.com>
2021-03-04 23:49:19 +0800

    homework1

---
+++ Homework1/HM1/HM1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HM1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            char op;
+            double a1,a2,ans = 0;
+            Console.WriteLine("input 2 numbers and 1 operator:");
+            s = Console.ReadLine();
+            a1 = Double.Parse(s);
+            s = Console.ReadLine();
+            a2 = Double.Parse(s);
+            s = Console.ReadLine();
+            op = char.Parse(s);
+            switch (op)
+            {
+                case '+':
+                    ans = a1 + a2;break;
+                case '-':
+                    ans = a1 - a2;break;
+                case '*':
+                    ans = a1 * a2;break;
+                case '/':
+                    ans = a1 / a2;break;
+                case '%':
+                    ans = a1 % a2;break;
+                default:
+                    Console.WriteLine("input error");return;
+            }
+            Console.WriteLine($"answer is {ans}");
+        }
+    }
+}
---
+++ Homework1/HM1/HM2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HM2
+{
+    public partial class Form1 : Form
+    {
+        string s1, s2,op;
+        double n1, n2, ans;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            op = comboBox1.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (op)
+            {
+                case "+": ans = n1 + n2; break;
+                case "-": ans = n1 - n2; break;
+                case "*": ans = n1 * n2; break;
+                case "/": ans = n1 / n2; break;
+                case "%": ans = n1 % n2; break;
+                default: ans = -10000; break;
+            }
+            textBox3.Text = Convert.ToString(ans);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            n1 =Convert.ToDouble(textBox1.Text);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            n2 = Convert.ToDouble(textBox2.Text);
+        }
+
+    }
+}
