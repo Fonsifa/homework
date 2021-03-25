69bec1a5df035b8499a6006b72aeabd89e7406c9
冯景琦 <1563771456@qq.com>
2021-03-25 17:07:49 +0800

    homework4

---
+++ homework4/homework4/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+public class Node<T>
+{
+    public Node<T> Next { get; set; }
+    public T Data { set; get; }
+
+    public Node(T t)
+    {
+        Next = null;
+        Data = t;
+    }
+}
+public class GenericList<T>
+{
+    public static int min_num = 1000000, max_num = -1000000, sum = 0;
+    private Node<T> head, tail;
+    public GenericList()
+    {
+        tail = head = null;
+    }
+    public Node<T> Head { get => head; }
+    public Node<T> Tail { get => tail; }
+    public void Add(T t)
+    {
+        Node<T> n = new Node<T>(t);
+        if (tail == null)
+        {
+            head = tail = n;
+        }
+        else
+        {
+            tail.Next = n;
+            tail = n;
+        }
+    }
+    public static void ForEach(GenericList<T> t,Action<T> action)
+    {
+        Node<T> a = t.Head;
+        while (a != null)
+        {
+            action(a.Data);
+            a = a.Next;
+        }
+    }
+}
+public delegate int F(GenericList<int> t);
+
+
+namespace homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> intlist = new GenericList<int>();
+            int n = Convert.ToInt32(Console.ReadLine());
+            for(int i=1;i<=n;i++)
+            {
+                int x = Convert.ToInt32(Console.ReadLine());
+                if (x > GenericList<int>.max_num) GenericList<int>.max_num = x;
+                if (x < GenericList<int>.min_num) GenericList<int>.min_num = x;
+                GenericList<int>.sum += x;
+                intlist.Add(x);
+            }
+            Action<int> ac = delegate (int i) { Console.WriteLine(i); };
+            GenericList<int>.ForEach(intlist, ac);
+            F minn = delegate (GenericList<int> t) { return GenericList<int>.min_num; };
+            F maxn= delegate (GenericList<int> t) { return GenericList<int>.max_num; };
+            F sumn=delegate(GenericList<int> t) { return GenericList<int>.sum; };
+            Console.WriteLine($"最小值为{minn(intlist)}");
+            Console.WriteLine($"最大值为{maxn(intlist)}");
+            Console.WriteLine($"总和为{sumn(intlist)}");
+        }
+    }
+}
---
+++ homework4/work2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+public delegate void AlarmHandler(object sender, AlarmEvents args);
+public class AlarmEvents
+{
+    public int time_now { set; get; }
+    public int alrmTime { set; get; }
+}
+
+public class Clock
+{ 
+    //public int Alarm_Time { set; get; }
+    //public int time_now { set; get; }
+    //public Clock(int x,int y)
+    //{
+    //    time_now = x;
+    //    Alarm_Time = y;
+    //}
+    public event AlarmHandler TICK, ALARM;
+    public void Tick(int time_now,int Alarm_Time)
+    {
+        Console.WriteLine($"现在的时间是{time_now}");
+        AlarmEvents args = new AlarmEvents() { time_now = time_now, alrmTime = Alarm_Time };
+        TICK(this, args);
+    }
+    public void Alarm(int time_now, int Alarm_Time)
+    {
+        Console.WriteLine($"现在的时间是{time_now}");
+        AlarmEvents args = new AlarmEvents() { time_now = time_now, alrmTime = Alarm_Time };
+        ALARM(this, args);
+    }
+}
+class Form
+{
+    public Clock c=new Clock();
+    public Form()
+    {
+        c.TICK += new AlarmHandler(clock_tick);
+        c.ALARM += new AlarmHandler(clock_alarm);
+    }
+    void clock_tick(object sender,AlarmEvents args)
+    {
+        Console.WriteLine($"tick tick");
+    }
+    void clock_alarm(object sender, AlarmEvents args)
+    {
+        Console.WriteLine($"alarm alarm");
+    }
+}
+
+namespace work2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form form1 = new Form();
+            int time_now = 100, Alarm_Time = 1000;
+            while(true)
+            {
+                if(time_now<Alarm_Time)
+                form1.c.Tick(time_now, Alarm_Time);
+                else
+                {
+                    form1.c.Alarm(time_now, Alarm_Time);
+                    if ((time_now - Alarm_Time) >= 10) break;
+                }
+                time_now++;
+            }
+        }
+    }
+}
